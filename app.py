f0eadf3fad8a617ea1a85cc0ec2e297f6c8b6dc0
iaffaan <affaanali5786@gmail.com>
2026-03-21 04:52:09 +0530

    Fix port for render

--- app.py
+++ app.py
@@ -374,4 +374,5 @@ def delete_photo(photo_id):
     return redirect(url_for('dashboard'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port=int(os.environ.get("PORT",10000))
+    app.run(host='0.0.0.0', port=port)
